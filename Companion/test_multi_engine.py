8fe44b24471f7f2fd12c065294a4c995a69e000d
shinraw55-ux <shinraw55@gmail.com>
2026-09-19 01:01:22 +0200

    Align engine tests with supported bundled runtime

--- Companion/test_multi_engine.py
+++ Companion/test_multi_engine.py
@@ -6,8 +6,10 @@ import tts_registry
 class MultiEngineTests(unittest.TestCase):
     def test_all_three_registered(self):
         self.assertEqual(set(tts_registry.SPECS), {"kokoro", "chatterbox", "cosyvoice"})
-    def test_unknown_falls_back_to_kokoro(self):
-        self.assertEqual(tts_registry.normalize_engine("wat"), "kokoro")
+    def test_unknown_falls_back_to_bundled_default(self):
+        self.assertEqual(tts_registry.normalize_engine("wat"), "chatterbox")
+    def test_only_installable_engine_is_exposed(self):
+        self.assertEqual(tts_registry.bundled_engine_keys(), ["chatterbox"])
     def test_labels_are_unique(self):
         vals=list(tts_registry.labels().values())
         self.assertEqual(len(vals), len(set(vals)))
